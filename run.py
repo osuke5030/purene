a945ff8ea7636746374d498efe203d483eb9e6e9
osuke <o-suke.0108@docomo.ne.jp>
2022-07-04 00:01:32 +0900

    commit

--- run.py
+++ run.py
@@ -14,52 +14,52 @@ import openpyxl
 
 
 def job():
-    # .envファイルを探して読み込み
-    env_file = find_dotenv()
-    load_dotenv(env_file)  
-
-    CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
-    CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
-    ACCESS_KEY = os.environ.get('ACCESS_KEY')
-    ACCESS_KEY_SECRET = os.environ.get('ACCESS_KEY_SECRET')
-
-    # Twitterの認証
-    twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_KEY_SECRET)
-    print(twitter)
-
-    #エンドポイント
-    url_text = 'https://api.twitter.com/1.1/statuses/update.json'
-    url_media = "https://upload.twitter.com/1.1/media/upload.json"
-
-
-
-    # ここまでTwitter投稿の準備
-    print("投稿準備完了")
-
-
-
-    URL = 'https://orenoraresne.com/'
-    # リクエストヘッダの指定
-    headers = {"User-Agent": "hoge"}
-    response = requests.get(URL,  headers=headers)
-    r_text=response.text
-    soup = BeautifulSoup(r_text, 'html.parser')
-
-
-    print('レアスニのページの取得完了')
-
-    soup_article=soup.find_all("article",attrs={"class","post-list"})[0]
-    soup_img=soup_article.find_all("img")[0]['src']
-    soup_name=soup_article.find_all("h1",attrs={"class","entry-title"})[0].text
-    soup_url=soup_article.find_all("a")[0]["href"]
-    # 画像の処理
-    response = requests.get(soup_img)
-    image = response.content
-    files = {"media" : image}
-    req_media = twitter.post(url_media, files = files)
-    media_id = json.loads(req_media.text)['media_id']
-    print('画像の取得完了')
     try:
+        # .envファイルを探して読み込み
+        env_file = find_dotenv()
+        load_dotenv(env_file)  
+
+        CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+        CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+        ACCESS_KEY = os.environ.get('ACCESS_KEY')
+        ACCESS_KEY_SECRET = os.environ.get('ACCESS_KEY_SECRET')
+
+        # Twitterの認証
+        twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_KEY_SECRET)
+        print(twitter)
+
+        #エンドポイント
+        url_text = 'https://api.twitter.com/1.1/statuses/update.json'
+        url_media = "https://upload.twitter.com/1.1/media/upload.json"
+
+
+
+        # ここまでTwitter投稿の準備
+        print("投稿準備完了")
+
+
+
+        URL = 'https://orenoraresne.com/'
+        # リクエストヘッダの指定
+        headers = {"User-Agent": "hoge"}
+        response = requests.get(URL,  headers=headers)
+        r_text=response.text
+        soup = BeautifulSoup(r_text, 'html.parser')
+
+
+        print('レアスニのページの取得完了')
+
+        soup_article=soup.find_all("article",attrs={"class","post-list"})[0]
+        soup_img=soup_article.find_all("img")[0]['src']
+        soup_name=soup_article.find_all("h1",attrs={"class","entry-title"})[0].text
+        soup_url=soup_article.find_all("a")[0]["href"]
+        # 画像の処理
+        response = requests.get(soup_img)
+        image = response.content
+        files = {"media" : image}
+        req_media = twitter.post(url_media, files = files)
+        media_id = json.loads(req_media.text)['media_id']
+        print('画像の取得完了')
         print("ここから詳細ページ")
         URL = soup_url
         # リクエストヘッダの指定
